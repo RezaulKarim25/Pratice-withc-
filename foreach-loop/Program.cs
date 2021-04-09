6f39d51bf5409536cee18a754a8d342c9e5ab8c8
RezaulKarim25 <81696433+RezaulKarim25@users.noreply.github.com>
2021-04-10 01:35:31 +0600

    Add files via upload

---
+++ foreach-loop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach_loop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] FigterAircapter = { "mick29", "Rafail-19", "F-15 Eagles ", "Mazda" };
+
+
+            foreach (string Airbus in FigterAircapter)
+
+            {
+                Console.WriteLine(Airbus);
+            }
+        //    int[] pins = new int[4] { 9, 3, 7, 2 };// Single dimentional array with int type.
+        //String[] Aircapter = { "mick29", "Rafail-19", " F-15 Eagles", "Mazda" };//Single dimentional array with string arry,
+        //var names = new[] {new {Name ="Hridoy",Age = 24},
+        //                      new {Name ="Dina",Age = 29},
+        //                      new {Name ="Rafin",Age = 14},
+        //                      new {Name ="Tahmenna",Age = 14}, };
+
+        //String[] array = new string[5];
+        //array[0] = "Hridoy";
+        //    array[1] = "Dina";
+        //    array[2] = "Rafin";
+        //    array[3] = "Tahmenna";
+        //    array[4] = "Binthy";
+
+        //    Console.WriteLine("All display array");
+        //    foreach (string slow in array)
+        //    {
+        //        Console.WriteLine(array);
+        //    }
+            Console.ReadKey();
+
+        }      
+    }
+    
+  
+    
+}

435e94a96bef11aa2577cee52e9a6686785523bd
RezaulKarim25 <81696433+RezaulKarim25@users.noreply.github.com>
2021-04-10 01:17:35 +0600

    Add files via upload

---
+++ Implementing-Interface/Horse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Interface
+{
+    
+      class Horse: ILandBond, IJourney
+      {
+            int ILandBond.NumberOfLegs()
+            {
+                return 4;
+            }
+            int IJourney.NumberOfLegs()
+            {
+                return 3;
+            }
+      }
+
+    
+}
---
+++ Implementing-Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Interface
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Horse horse = new Horse();
+            //int legs = horse.NumberOfLegs();
+            //Not Compile
+
+            IJourney journeyHorse = horse;
+            int legsInJourney = journeyHorse.NumberOfLegs();
+            Console.WriteLine(legsInJourney);
+
+            ILandBond landBondHorse = horse;
+            int legsOnHorse = landBondHorse.NumberOfLegs();
+            Console.WriteLine(legsOnHorse);
+            Console.ReadKey();
+        }       
+    }
+}

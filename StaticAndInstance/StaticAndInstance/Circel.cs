492a417c4f6fdc72b02160790cc3ccf0c0eaec7d
RezaulKarim25 <81696433+RezaulKarim25@users.noreply.github.com>
2021-04-10 01:16:00 +0600

    Add files via upload

---
+++ StaticAndInstance/StaticAndInstance/Circel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAndInstance
+{
+   class Circel // This is static class.
+   {
+        public static float pi; // This is static member of class.
+        int Radius; // This is instince menber of class.
+        static Circel()  // This is static constractor. static constractor doesn't allow peramettar.
+        {
+            pi = 3.1416F; // Static Constractor value assigend .
+        }
+        public Circel(int radius) // Parameterise Constractor. Instance constractor allow perametter.
+        {
+            this.Radius = radius;
+
+        }
+        public void getResult()
+        {
+            Console.WriteLine("Calculated result is: {0}",pi* Radius*Radius);
+        }
+            
+
+   }
+
+}
---
+++ StaticAndInstance/StaticAndInstance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAndInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circel cr = new Circel(13);
+            cr.getResult();
+            Circel ch = new Circel(13);
+            ch.getResult();
+            Circel ch1 = new Circel(13);
+            ch1.getResult();
+            Circel ch2 = new Circel(13);
+            ch2.getResult();
+            Console.ReadKey();
+            // application of static class member. It's not possible for multitimes call. It's use for single time.
+            Console.WriteLine("value of pi is: {0}",Circel.pi);
+            Console.ReadKey();
+        }
+
+    }
+}
